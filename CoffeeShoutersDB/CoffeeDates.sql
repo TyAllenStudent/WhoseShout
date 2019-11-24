01db730023a74a398443196a34c780041ac8420a
TY ALLEN <102058205@student.swin.edu.au>
2019-11-24 18:18:04 +1100

    Add project files.

---
+++ CoffeeShoutersDB/CoffeeDates.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CoffeeDates]
+(
+	[Shouter] INT NOT NULL,
+	[Date] Date NOT NULL,
+	[Time] Time NOT NULL,
+	[Venue] NVARCHAR (20) NOT NULL,
+	[Cost] Money,
+	CONSTRAINT FK_Shouter_Id FOREIGN KEY (Shouter) REFERENCES Users (Id)
+
+)
